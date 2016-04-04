da822809c952bd30b683757a4179a79718452aaf
agconti <agc11d@gmail.com>
2016-04-03 21:20:36 -0400

    fix(bubble_sort/bubble_sort): updated indexing and comparators

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -2,19 +2,18 @@
 
 
 def bubble_sort(items):
-    number_of_sweeps_left = len(items) - 1
+    number_of_sweeps_left = len(items)
 
     while number_of_sweeps_left > 0:
+        number_of_sweeps_left -= 1
 
         for index, item in enumerate(items):
-            if index >= number_of_sweeps_left:
+            if index == number_of_sweeps_left:
                 break
 
             next_item = items[index + 1]
             if item > next_item:
                 items[index] = next_item
                 items[index + 1] = item
-
-        number_of_sweeps_left -= 1
 
     return items
