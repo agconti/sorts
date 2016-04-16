ee96de2b2f5cf210c6701f6310733587c438c763
agconti <agc11d@gmail.com>
2016-04-16 18:01:18 -0400

    perf(bubble_sort/bubble_sort): allowed exit after ability to swap is exahausted

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,20 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # https://youtu.be/P00xJgWzz2c?list=PLAB1DA9F452D9466C
 # f(n) = c + n x c x n x c x c x c x c x c
 # O(n^2), Θ(n^2), Ω(n^2)
 
+
 def bubble_sort(items):
-    number_of_sweeps_left = len(items)
+    swapped = True
+    sweeps_left = len(items)
 
-    while number_of_sweeps_left > 0:
-        number_of_sweeps_left -= 1
+    while sweeps_left > 0 and swapped:
+        swapped = False
+        sweeps_left -= 1
 
-        for index, item in enumerate(items):
-            if index == number_of_sweeps_left:
-                break
+        for index in range(sweeps_left):
 
+            item = items[index]
             next_item = items[index + 1]
             if item > next_item:
                 items[index] = next_item
                 items[index + 1] = item
+                swapped = True
 
     return items
